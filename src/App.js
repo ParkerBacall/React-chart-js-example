ff01596ec730bafd88c1299baf9657ce1dd9ed43
ParkerBacall <pbacall2@gmail.com>
2020-02-09 19:34:46 -0700

    example of react app using chart.js with a chart component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Chart from './components/Chart'
 import './App.css';
 
-function App() {
+
+class App extends Component {
+
+  state = {
+    chartData: {}
+  }
+
+  componentWillMount(){
+    this.getChartData()
+  }
+
+  getChartData(){
+    this.setState({
+      //fetch here
+      chartData:  {labels: ['Boston', 'Worchester', 'Springfeild', 'Lowell', 'Cambridge', 'New Bedford'],
+      datasets:[
+        {
+          label: 'Populations',
+          data:[
+            617594,
+            181045,
+            153060,
+            106519,
+            105162,
+            95072
+          ],
+          backgroundColor:[
+            "rgb(255, 166, 249)",
+            "rgb(161, 202, 237)",
+            "rgb(247, 219, 141)",
+            "rgb(131, 247, 166",
+            "rgb(196, 141, 247)",
+            "rgb(247, 150, 141)"
+          ]
+        }
+      ]
+    }
+    })
+  }
+  
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Chart location={'Massachuetts'} chartData={this.state.chartData} legendPosition='bottom'/>
+        <Chart location={'Colorado'} chartData={this.state.chartData} legendPosition='top'/>
     </div>
   );
+}
 }
 
 export default App;
